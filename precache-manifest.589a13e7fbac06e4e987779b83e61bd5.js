9814f3f1378ea20406daf9296584368c5e49f5c3
Mike Sandula <msandula@gmail.com>
2019-12-05 18:18:02 -0500

    Updates

---
+++ precache-manifest.589a13e7fbac06e4e987779b83e61bd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7204c9c3291f917f85e1f6cdc73fdc82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e26171f8b2ee43b58f13",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "84d1c82b91b93f79b238",
+    "url": "/static/js/2.cfb92d69.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cfb92d69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e26171f8b2ee43b58f13",
+    "url": "/static/js/main.c537392d.chunk.js"
+  },
+  {
+    "revision": "2be9b04573260cd388f8",
+    "url": "/static/js/runtime-main.811551dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
