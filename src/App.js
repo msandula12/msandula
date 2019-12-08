63d4a7167a1b11f854b9d4cbd57ee86077914302
Mike Sandula <msandula@gmail.com>
2019-12-08 15:26:58 -0500

    feat: page basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Page from './components/Page';
-
-import About from './views/About';
-import Resume from './views/Resume';
-
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Page>
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/resume" component={Resume} />
-          </Switch>
-        </Page>
+    <div className="my-app">
+      {/* LANDING */}
+      <div id="home" className="page landing">
+        <div className="landing-logo">
+          <div className="my-name">
+            <span className="code-operator">{'<'}</span>
+            <span className="code-class">{'MikeSandula'}</span>
+            <span className="code-operator">{' /> '}</span>
+            <span className="code-field">{'{'}</span>
+            <span className="code-singleLineComment">
+              {'/* Software Developer */'}
+            </span>
+            <span className="code-field">{'}'}</span>
+          </div>
+        </div>
+        <div className="scroll-down arrow-down">
+          <a className="arrow" href="#about">
+            {'>'}
+          </a>
+        </div>
       </div>
-    </Router>
+
+      {/* ABOUT */}
+      <div id="about" className="page about">
+        <div>
+          <h1>About</h1>
+          <div className="content">
+            After graduating from Oakland University in 2010 with a Bachelor of
+            Arts in Journalism, Mike Sandula worked for several years as a copy
+            editor. But eventually he found his true passion in coding.
+          </div>
+        </div>
+      </div>
+
+      {/* RESUME */}
+      <div id="resume" className="page resume">
+        <div>
+          <h1>Resume</h1>
+          <div className="content">{`{/* TODO *}`}</div>
+        </div>
+      </div>
+
+      {/* HEADER */}
+      <div className="header"></div>
+    </div>
   );
 };
 
