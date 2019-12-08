7ec0200513021d31b364eeadc1139deb182eaee3
Mike Sandula <msandula@gmail.com>
2019-12-08 10:57:29 -0500

    feat: add react-typist

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Typist from 'react-typist';
+
 import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className="code">
+    <Typist
+      className="code my-name"
+      avgTypingDelay={200}
+      cursor={{ hideWhenDone: true, hideWhenDoneDelay: 0 }}
+      startDelay={2000}
+      stdTypingDelay={100}
+    >
       <span className="code-operator">{'<'}</span>
       <span className="code-class">{'MikeSandula'}</span>
       <span className="code-operator">{' /> '}</span>
@@ -12,7 +20,7 @@ const Logo = () => {
         {'/* Software Developer */'}
       </span>
       <span className="code-field">{'}'}</span>
-    </div>
+    </Typist>
   );
 };
 
