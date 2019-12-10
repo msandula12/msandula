47903c13500850a77d749175035fc2f094b26e78
Mike Sandula <msandula@gmail.com>
2019-12-10 18:55:50 -0500

    feat: only show header when home page not in view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 
 import './App.css';
 
@@ -6,13 +6,30 @@ import { ResumeText } from './Text';
 
 const App = () => {
   const [page, setPage] = useState('');
+  const [shouldShowHeader, setShouldShowHeader] = useState(false);
 
   const year = new Date().getFullYear();
+
+  const homePage = createRef();
+
+  useEffect(() => {
+    const determineIfHomePageIsOutOfView = () => {
+      if (!homePage.current) {
+        return;
+      }
+      const rect = homePage.current.getBoundingClientRect();
+      const { height, top } = rect;
+      setShouldShowHeader(top + height <= 0);
+    };
+    window.addEventListener('scroll', determineIfHomePageIsOutOfView);
+    return () =>
+      window.removeEventListener('scroll', determineIfHomePageIsOutOfView);
+  }, [homePage]);
 
   return (
     <div className="my-app">
       {/* LANDING */}
-      <div id="/home" className="page landing">
+      <div id="/home" ref={homePage} className="page landing">
         <div className="landing-logo">
           <div className="my-name">
             <span className="code-operator">{'<'}</span>
@@ -33,43 +50,45 @@ const App = () => {
       </div>
 
       {/* HEADER */}
-      <header className="header">
-        <div className="flex-1">
-          <div className="my-name">
-            <span className="code-operator">{'<'}</span>
-            <span className="code-class">{'MikeSandula'}</span>
-            <span className="code-operator">{' /> '}</span>
-            <span className="code-field">{'{'}</span>
-            <span className="code-singleLineComment">
-              {'/* Software Developer */'}
-            </span>
-            <span className="code-field">{'}'}</span>
+      {shouldShowHeader && (
+        <header>
+          <div className="flex-1">
+            <div className="my-name">
+              <span className="code-operator">{'<'}</span>
+              <span className="code-class">{'MikeSandula'}</span>
+              <span className="code-operator">{' /> '}</span>
+              <span className="code-field">{'{'}</span>
+              <span className="code-singleLineComment">
+                {'/* Software Developer */'}
+              </span>
+              <span className="code-field">{'}'}</span>
+            </div>
           </div>
-        </div>
-        <div className="nav">
-          <a
-            href="#/about"
-            onClick={() => setPage('about')}
-            className={page === 'about' ? 'active' : ''}
-          >
-            About
-          </a>
-          <a
-            href="#/resume"
-            onClick={() => setPage('resume')}
-            className={page === 'resume' ? 'active' : ''}
-          >
-            Resume
-          </a>
-        </div>
-        <div className="flex-1 scroll-up">
-          <div className="arrow rotate-90 float-right">
-            <a href="#/home" onClick={() => setPage('home')}>
-              {'<'}
+          <div className="nav">
+            <a
+              href="#/about"
+              onClick={() => setPage('about')}
+              className={page === 'about' ? 'active' : ''}
+            >
+              About
+            </a>
+            <a
+              href="#/resume"
+              onClick={() => setPage('resume')}
+              className={page === 'resume' ? 'active' : ''}
+            >
+              Resume
             </a>
           </div>
-        </div>
-      </header>
+          <div className="flex-1 scroll-up">
+            <div className="arrow rotate-90 float-right">
+              <a href="#/home" onClick={() => setPage('home')}>
+                {'<'}
+              </a>
+            </div>
+          </div>
+        </header>
+      )}
 
       {/* ABOUT */}
       <div id="/about" className="page about">
