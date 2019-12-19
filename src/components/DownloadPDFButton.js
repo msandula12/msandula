d2033dd78a1d045fb47156ea6360712cc967b0aa
Mike Sandula <msandula@gmail.com>
2019-12-19 17:41:48 -0500

    feat: use react-spring to animate PDF button

--- src/components/DownloadPDFButton.js
+++ src/components/DownloadPDFButton.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 
 import './DownloadPDFButton.css';
 
 const DownloadPDFButton = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const hoverProps = useSpring({
+    boxShadow: isHovering
+      ? '0 0 16px 0 rgba(255, 255, 255, 0.3)'
+      : '0 0 8px 0 rgba(255, 255, 255, 0.2)',
+    transform: isHovering ? 'scale(1.1)' : 'scale(1)'
+  });
+
   return (
-    <div
+    <animated.div
       className="download-pdf"
       onClick={() =>
         window.open(require('../assets/files/mike-sandula-resume.pdf'), '_none')
       }
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      style={hoverProps}
     >
       View as PDF
-    </div>
+    </animated.div>
   );
 };
 
