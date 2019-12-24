1b26f39cfd96de12c0c8f356291ed1fecb7b9204
Mike Sandula <msandula@gmail.com>
2019-12-24 12:27:30 -0500

    feat: set up basics for menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { scrollToPage } from '../utils/helpers';
 
 import './Header.css';
 
 const Header = ({ activePage }) => {
+  const [canShowMenu, setCanShowMenu] = useState(true);
+  const [isShowingMenu, setIsShowingMenu] = useState(true);
+
   const pages = [
     {
       label: 'About',
@@ -15,6 +18,17 @@ const Header = ({ activePage }) => {
       page: 'resume'
     }
   ];
+
+  const handlePageNav = page => {
+    if (canShowMenu && isShowingMenu) {
+      closeMenu();
+    }
+    scrollToPage(page);
+  };
+
+  const closeMenu = () => {
+    setIsShowingMenu(false);
+  };
 
   const getPositionOfUnderline = () => {
     const index = activePage
@@ -37,26 +51,48 @@ const Header = ({ activePage }) => {
           <span className="code-field">{'}'}</span>
         </div>
       </div>
-      <nav>
-        {pages.map(page => (
-          <a
-            href={`#/${page.page}`}
-            key={page.page}
-            onClick={() => scrollToPage(page.page)}
-            style={{ width: `100 / ${pages.length}%` }}
-          >
-            {page.label}
-          </a>
-        ))}
-        <hr style={{ marginLeft: `${getPositionOfUnderline()}%` }} />
-      </nav>
-      <div className="flex-1 scroll-up">
+      {!canShowMenu && (
+        <nav>
+          {pages.map(page => (
+            <a
+              href={`#/${page.page}`}
+              key={page.page}
+              onClick={() => handlePageNav(page.page)}
+              style={{ width: `100 / ${pages.length}%` }}
+            >
+              {page.label}
+            </a>
+          ))}
+          <hr style={{ marginLeft: `${getPositionOfUnderline()}%` }} />
+        </nav>
+      )}
+      <div className="nav-icon">
         <div className="arrow rotate-90 float-right">
-          <a href="#/home" onClick={() => scrollToPage('home')}>
+          <a href="#/home" onClick={() => handlePageNav('home')}>
             {'<'}
           </a>
         </div>
       </div>
+
+      {isShowingMenu && (
+        <div className="menu">
+          <div onClick={closeMenu} className="menu-close">
+            X
+          </div>
+          <nav>
+            {pages.map(page => (
+              <a
+                href={`#/${page.page}`}
+                key={page.page}
+                onClick={() => handlePageNav(page.page)}
+                style={{ width: `100 / ${pages.length}%` }}
+              >
+                {page.label}
+              </a>
+            ))}
+          </nav>
+        </div>
+      )}
     </header>
   );
 };
