403ba6dd93dd1545eab5f6c87fa79311f4d06ad3
Mike Sandula <msandula@gmail.com>
2019-12-25 10:40:54 -0500

    feat: add link to source code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,20 @@ import './Footer.css';
 const Footer = () => {
   const year = new Date().getFullYear();
 
-  return <footer>Copyright &copy;{year} Mike Sandula</footer>;
+  return (
+    <footer>
+      View the source code at{' '}
+      <a
+        className="code-field"
+        href="https://github.com/msandula12/msandula"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        github.com/msandula12/msandula
+      </a>
+      . Copyright &copy;{year} Mike Sandula.
+    </footer>
+  );
 };
 
 export default Footer;
