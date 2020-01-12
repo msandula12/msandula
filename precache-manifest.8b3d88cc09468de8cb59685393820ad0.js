d168a9faad4f5236f7e9e9c4847f372146513c7f
Mike Sandula <msandula@gmail.com>
2020-01-12 10:45:52 -0500

    Updates

---
+++ precache-manifest.8b3d88cc09468de8cb59685393820ad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d13d3a3fdbb6aeba979c071bfcd6177",
+    "url": "/msandula/index.html"
+  },
+  {
+    "revision": "ff0cb54f03252b7f9ce3",
+    "url": "/msandula/static/css/main.bf5a51ac.chunk.css"
+  },
+  {
+    "revision": "5b2abe37476305d0e0c4",
+    "url": "/msandula/static/js/2.ecba26bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/msandula/static/js/2.ecba26bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff0cb54f03252b7f9ce3",
+    "url": "/msandula/static/js/main.1d481478.chunk.js"
+  },
+  {
+    "revision": "df729c8691e309ffb25e",
+    "url": "/msandula/static/js/runtime-main.633b504b.js"
+  },
+  {
+    "revision": "4234868017752ce82135893383d75f22",
+    "url": "/msandula/static/media/mike-sandula-resume.42348680.pdf"
+  }
+]);
